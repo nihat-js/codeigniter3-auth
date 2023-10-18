6cca4d29ec5e647a7a5a1e255fb0f56ef4fb8853
Nihat <abdullazadenihat@gmail.com>
2023-10-18 16:29:40 +0400

    init commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Test extends CI_Controller {
+
+	/**
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/userguide3/general/urls.html
+	 */
+	public function index()
+	{
+    echo "oha";
+	}
+}
